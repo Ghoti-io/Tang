4acc9e4381ca00b94c262741855d678098034fd0
Corey Pennycuff <pennycuff.c@gmail.com>
2022-10-31 20:59:34 -0500

    Added semicolon statements to the language (forgot the AstNodeBlock class files)

---
+++ include/astNodeBlock.hpp
@@ -0,0 +1,40 @@
+/**
+ * @file
+ * Declare the Tang::AstNodeBlock class.
+ */
+
+#ifndef TANG_ASTNODEBLOCK_HPP
+#define TANG_ASTNODEBLOCK_HPP
+
+#include <vector>
+#include <memory>
+#include "astNode.hpp"
+
+namespace Tang {
+  /**
+   * An AstNode that represents a code block.
+   */
+  class AstNodeBlock : public AstNode {
+  public:
+    /**
+     * The constructor.
+     *
+     * @param statements The statements of the code block.
+     * @param location The location associated with the expression.
+     */
+    AstNodeBlock(const std::vector<std::shared_ptr<AstNode>> & statements, Tang::location location);
+
+    virtual std::string dump(std::string indent = "") const override;
+    virtual void compile(Tang::Program & program) const override;
+    virtual shared_ptr<AstNode> makeCopy() const override;
+
+  private:
+    /**
+     * The statements included in the code block.
+     */
+    std::vector<std::shared_ptr<AstNode>> statements;
+  };
+}
+
+#endif // TANG_ASTNODEBLOCK_HPP
+
---
+++ src/astNodeBlock.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file
+ * Define the Tang::AstNodeBlock class.
+ */
+
+#include <bit>
+#include "astNodeBlock.hpp"
+#include "opcode.hpp"
+
+using namespace std;
+using namespace Tang;
+
+AstNodeBlock::AstNodeBlock(const vector<shared_ptr<AstNode>> & statements, Tang::location location) : AstNode(location), statements{statements} {}
+
+string AstNodeBlock::dump(string indent) const {
+  string out = indent + "Block:\n";
+  for (auto x : this->statements) {
+    out += x->dump(indent + "  ");
+  }
+  return out;
+}
+
+void AstNodeBlock::compile(Tang::Program & program) const {
+  if (this->statements.size()) {
+    // Compile all but the last statement, adding POP after the statement.
+    for (size_t i = 0; i < this->statements.size() - 1; ++i) {
+      this->statements[i]->compile(program);
+      program.addBytecode((uint64_t)Opcode::POP);
+    }
+    // Compile the last statement without a POP so that its value is left on
+    // the stack.
+    this->statements.back()->compile(program);
+  }
+}
+
+shared_ptr<AstNode> AstNodeBlock::makeCopy() const {
+  return make_shared<AstNodeBlock>(this->statements, this->location);
+}
+
