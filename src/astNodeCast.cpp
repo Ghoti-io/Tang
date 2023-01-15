f3208ee2c28a22265cde722c7bae19b66ff839ac
Corey Pennycuff <pennycuff.c@gmail.com>
2023-01-14 18:20:33 -0600

    Simple refactor to use S/I macros when compiling AstNodeCast

--- src/astNodeCast.cpp
+++ src/astNodeCast.cpp
@@ -7,39 +7,12 @@
 #include "astNodeCast.hpp"
 #include "astNodeIdentifier.hpp"
 #include "astNodeString.hpp"
+#include "macros.hpp"
 #include "opcode.hpp"
 #include "program.hpp"
 
 using namespace std;
 using namespace Tang;
-
-#define CASTOP(OP_S, OP_I) \
-  integer_t index{-1}; \
-  auto & identifier = program.getIdentifiers(); \
-  if (typeid(*this->expression) == typeid(AstNodeIdentifier)) { \
-    auto & name = static_cast<AstNodeIdentifier &>(*this->expression).name; \
-    if (identifier.count(name)) { \
-      index = identifier.at(name); \
-    } \
-  } \
-  else if (typeid(*this->expression) == typeid(AstNodeString)) { \
-    auto & strings = program.getStrings(); \
-    auto & stringConv = static_cast<AstNodeString &>(*this->expression); \
-    auto & val = stringConv.getVal(); \
-    auto & type = stringConv.getType(); \
-    if (strings.count({val, type})) { \
-      index = strings.at({val, type}) + program.getIdentifiers().size(); \
-    } \
-  } \
-  if (index >= 0) { \
-    program.addBytecode((uinteger_t)Opcode:: OP_I); \
-    program.addBytecode((uinteger_t)index); \
-  } \
-  else { \
-    this->expression->compile(program); \
-    program.addBytecode((uinteger_t)Opcode:: OP_S); \
-  } \
-  return;
 
 AstNodeCast::AstNodeCast(Type targetType, shared_ptr<AstNode> expression, Tang::location location) : AstNode(location), targetType{targetType}, expression{expression} {}
 
@@ -60,19 +33,19 @@ void AstNodeCast::compilePreprocess(Program & program, PreprocessState state) co
 void AstNodeCast::compile(Tang::Program & program) const {
   switch (this->targetType) {
     case Integer: {
-      CASTOP(CASTINTEGER_S, CASTINTEGER_I);
+      UNARYOP(this->expression, CASTINTEGER_S, CASTINTEGER_I);
       break;
     }
     case Float: {
-      CASTOP(CASTFLOAT_S, CASTFLOAT_I);
+      UNARYOP(this->expression, CASTFLOAT_S, CASTFLOAT_I);
       break;
     }
     case Boolean: {
-      CASTOP(CASTBOOLEAN_S, CASTBOOLEAN_I);
+      UNARYOP(this->expression, CASTBOOLEAN_S, CASTBOOLEAN_I);
       break;
     }
     case String: {
-      CASTOP(CASTSTRING_S, CASTSTRING_I);
+      UNARYOP(this->expression, CASTSTRING_S, CASTSTRING_I);
       break;
     }
   }
