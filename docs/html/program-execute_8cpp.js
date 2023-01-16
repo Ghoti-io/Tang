9bb2f90074b27782f575e688fc876c43445a85b0
Corey Pennycuff <pennycuff.c@gmail.com>
2023-01-15 19:32:52 -0600

    Update docs for Episode 96

--- docs/html/program-execute_8cpp.js
+++ docs/html/program-execute_8cpp.js
@@ -1,9 +1,9 @@
 var program_execute_8cpp =
 [
-    [ "BINARYOP_II", "program-execute_8cpp.html#a56c11065adb02dd1e06bf6d5c0a3f1db", null ],
-    [ "BINARYOP_IS", "program-execute_8cpp.html#ad147b34a8bcfda75b4e0de785feceb86", null ],
-    [ "BINARYOP_SI", "program-execute_8cpp.html#a43193c2b4f59bd0e8e1479f6b7bfaaa9", null ],
-    [ "BINARYOP_SS", "program-execute_8cpp.html#a2e4329998787cb6fd62bf47732a09f75", null ],
+    [ "BINARYOP_II", "program-execute_8cpp.html#a4c8ed56b3d79a2ce0d152ad89eabc659", null ],
+    [ "BINARYOP_IS", "program-execute_8cpp.html#a7eff6d91569bef15306f8a8065bb8904", null ],
+    [ "BINARYOP_SI", "program-execute_8cpp.html#a2f53316cb83aec3c419de7f8dec43b6f", null ],
+    [ "BINARYOP_SS", "program-execute_8cpp.html#aef2846e8b21d4b7e111b2a3d86a75675", null ],
     [ "CASTOP_I", "program-execute_8cpp.html#a45f1dc1dd6b0559489c8f5bbbebd477a", null ],
     [ "CASTOP_S", "program-execute_8cpp.html#a0dcde88dabdc927ee94e2b87e126b601", null ],
     [ "EXECUTEPROGRAMCHECK", "program-execute_8cpp.html#a9ded9c6308c1d34a5872db3199e9c973", null ],
